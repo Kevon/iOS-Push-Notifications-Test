fa3988462fb5e7fffcd298161672013e00dd4d7c
Kevin Skompinski <kevons5252@gmail.com>
2017-08-31 21:07:44 -0400

    Initial Commit

--- Push It to the Limit/AppDelegate.m
+++ Push It to the Limit/AppDelegate.m
@@ -17,29 +17,23 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    application.applicationIconBadgeNumber = 0;
+    UILocalNotification *localNotif = launchOptions[UIApplicationLaunchOptionsLocalNotificationKey];
+    if(localNotif){
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"It's Time for a Notification" message:@"Launched from the background." preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *button = [UIAlertAction actionWithTitle:@"Go Away" style:UIAlertActionStyleDefault handler:nil];
+        [alert addAction:button];
+        dispatch_async(dispatch_get_main_queue(), ^{[application.keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];});
+    }
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+- (void) application:(UIApplication *)application didReceiveLocalNotification:(nonnull UILocalNotification *)notification{
+    application.applicationIconBadgeNumber = 0;
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"It's Time for a Notification" message:@"Now we're in the foreground." preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *button = [UIAlertAction actionWithTitle:@"Go Away" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:button];
+    dispatch_async(dispatch_get_main_queue(), ^{[application.keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];});
 }
 
 @end
--- Push It to the Limit/ViewController.m
+++ Push It to the Limit/ViewController.m
@@ -10,9 +10,30 @@
 
 @interface ViewController ()
 
+- (void) requestPermission;
+- (void) createNotification:(int)secondsInThefuture;
+
 @end
 
 @implementation ViewController
+
+- (void) requestPermission{
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+}
+
+- (void) createNotification:(int)secondsInThefuture{
+    UILocalNotification *notif = [[UILocalNotification alloc] init];
+    notif.fireDate = [[NSDate date] dateByAddingTimeInterval:secondsInThefuture];
+    notif.timeZone = nil;
+    notif.alertTitle = @"15 Seconds Has Passed";
+    notif.alertBody = @"Click here to further the plot.";
+    notif.alertAction = @"Go Away";
+    notif.soundName = UILocalNotificationDefaultSoundName;
+    notif.applicationIconBadgeNumber = 1738;
+    [[UIApplication sharedApplication] scheduleLocalNotification:notif];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -22,6 +43,11 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)ButtonTapped:(id)sender {
+    [self requestPermission];
+    [self createNotification:15];
 }
 
 @end
